977c6bb62f9681ea8a7b8e6cfab782fd0578e25c
shoaibgurmani98 <shoaibgurmani98@gmail.com>
2024-10-23 08:12:17 -0700

    summary test case resolved

--- titanic_analysis/summary_table.py
+++ titanic_analysis/summary_table.py
@@ -5,16 +5,16 @@ def create_summary_table(df):
         'Feature Name': df.columns,
         'Data Type': df.dtypes,
         'Number of Unique Values': [df[col].nunique() for col in df.columns],
-        'Missing Values': [df[col].isnull().sum() for col in df.columns]
+        'Missing Values': [df[col].isnull().sum() for col in df.columns],
     }
     
-    #summary DataFrame summary
+    # Create summary DataFrame
     summary_df = pd.DataFrame(summary_data)
     
     # Handle Missing Values
-    summary_df['Has Missing Values'] = summary_df['Missing Values'].apply(lambda x: 'Yes' if x > 0 else 'No')
+    summary_df['Has Missing Values?'] = summary_df['Missing Values'].apply(lambda x: 'Yes' if x > 0 else 'No')
     
-    # Drop missing values
+    # Drop the 'Missing Values' column as it's no longer needed
     summary_df.drop(columns='Missing Values', inplace=True)
     
     return summary_df
